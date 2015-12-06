aaff94ed0bb19df31359bfc92eac8995f22c4731
Lukas Gemar <Lukas Gemar>
2015-12-05 19:36:40 -0500

    Cleaned up directory structure

--- Kinect/playVideo.m
+++
@@ -1,9 +0,0 @@
-function [ ] = playVideo( video )
-%playVideo Play a kinect video
-%   Detailed explanation goes here
-
-for i = 1:size(video, 4)
-    imagesc(video(:, :, :, i)); 
-    pause(1/30); 
-end
-
--- Kinect/trial_acquisition.m
+++
@@ -1,5 +0,0 @@
-num_frames = 500; 
-% SS = small straight
-% MS = medium straight
-file_name = 'test_MS_2'; 
-recordKinectVideo(num_frames, file_name); 
